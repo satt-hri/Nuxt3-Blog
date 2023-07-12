da3d7f0b3cc35cd3f046f1515296186d099aef24
h_ri <h_ri@satt.jp>
2023-07-12 18:52:20 +0900

    tailwind css

--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,10 @@
 <template>
     <div class="w-full max-w-3xl mx-auto py-8">
         <div class="text-xl  font-semibold"> Home page</div>
+        <div class=" relative flex justify-end mb-7">
+            <button type="button"
+                class="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">Default</button>
+        </div>
     </div>
 </template>
 <script setup lang="ts">
